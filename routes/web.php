25f9f34055497ea8a27756814626eb188ac5e81f
Erven-Joshua-Cabral <erven.cabral@awsys-i.com>
2023-09-20 15:28:43 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show']);
 
-Route::get('overview', ResourceController::class)->only(['index']);
+Route::get('overview', [ResourceController::class, 'index'])->name('overview.index');
+
